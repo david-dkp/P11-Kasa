19cf02ccf56391b5eb067113cf942d0e108d108d
David <david.dekeuwer@gmail.com>
2022-07-09 11:45:37 +0200

    feat: create HomePage banner

--- src/containers/HomePage/components/HomePage.js
+++ src/containers/HomePage/components/HomePage.js
@@ -1,10 +1,21 @@
 import AccommodationThumbnail from "components/AccommodationThumbnail"
 import { Link } from "react-router-dom"
+import HomeBackgroundUrl from "assets/home-background.png"
 import "../styles.scss"
 
 const HomePage = ({ accommodations }) => {
     return (
-        <div>
+        <div className="container">
+            <div className="banner">
+                <img
+                    className="banner__image"
+                    src={HomeBackgroundUrl}
+                    alt="Paysage"
+                />
+                <h1 className="banner__title">
+                    Chez vous, partout et ailleurs
+                </h1>
+            </div>
             <div className="accommodations-container">
                 {accommodations.map((accommodation) => (
                     <Link
--- src/containers/PageLayout/components/Header.js
+++ src/containers/PageLayout/components/Header.js
@@ -5,31 +5,33 @@ const Header = () => {
     const location = useLocation()
     return (
         <header>
-            <Link className="home-logo-link" to={"/"}>
-                <LogoIcon className="logo-img" alt="Kasa logo" />
-            </Link>
-            <ul>
-                <li>
-                    <Link
-                        to={"/"}
-                        className={`link ${
-                            location.pathname === "/" && "active"
-                        }`}
-                    >
-                        Accueil
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to={"/about"}
-                        className={`link ${
-                            location.pathname === "/about" && "active"
-                        }`}
-                    >
-                        About
-                    </Link>
-                </li>
-            </ul>
+            <div className="header-container">
+                <Link className="home-logo-link" to={"/"}>
+                    <LogoIcon className="logo-img" alt="Kasa logo" />
+                </Link>
+                <ul>
+                    <li>
+                        <Link
+                            to={"/"}
+                            className={`link ${
+                                location.pathname === "/" && "active"
+                            }`}
+                        >
+                            Accueil
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to={"/about"}
+                            className={`link ${
+                                location.pathname === "/about" && "active"
+                            }`}
+                        >
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </header>
     )
 }
